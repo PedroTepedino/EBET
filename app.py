7b3774bc6673459bf49aecea17ee6ee4135fc999
tepe <pedrotepedinossauro@gmail.com>
2021-12-01 22:45:34 -0300

    Fix Password hash

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def cadastro2():
         msg += "As senhas nao são iguais\n"
 
     if is_valid_request:
-        comando = "INSERT INTO apostador(nmecomp_ap, datanasc_ap, cpf_ap, email_ap, username_ap, SHA(senha_ap)) VALUES (%s, %s, %s, %s, %s, %s);"
+        comando = "INSERT INTO apostador(nmecomp_ap, datanasc_ap, cpf_ap, email_ap, username_ap, senha_ap) VALUES (%s, %s, %s, %s, %s, SHA(%s));"
         if mysql.executar(comando, [nome, data, cpf, email, username, senha]):
             msg = "Cadastro realizado com sucesso!"
         else:
